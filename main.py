0ade1c328850d3ecc208cd07d4d17272e1357af4
edwardwang4340 <eddywang530@gmail.com>
2023-12-31 16:27:49 -0500

    built and trained the bird classification model
    TODO: add more species of birds for a fully functional model

--- main.py
+++ main.py
@@ -5,56 +5,110 @@ import numpy as np
 import PIL
 from PIL import Image
 import tensorflow as tf
-
 from tensorflow import keras
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.models import model_from_json
 
 data = []
 labels = []
 
-canada_goose = os.listdir("canada_goose")
+# creating dataset for canada goose
+canada_goose = os.listdir("ML - Bird Classifier/canada_goose")
 for goose in canada_goose:
-    imag=cv2.imread("canada_goose/"+goose)
+    imag = cv2.imread("ML - Bird Classifier/canada_goose/"+goose)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((50, 50))
     data.append(np.array(resized_image))
     labels.append(0)
 
-blue_jay = os.listdir("blue_jay")
+# creating dataset for blue jay
+blue_jay = os.listdir("ML - Bird Classifier/blue_jay")
 for jay in blue_jay:
-    imag=cv2.imread("blue_jay/"+blue_jay)
+    imag = cv2.imread("ML - Bird Classifier/blue_jay/"+jay)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((50, 50))
     data.append(np.array(resized_image))
     labels.append(1)
 
-northern_cardinal = os.listdir("northern_cardinal")
+# creating dataset for northern cardinal
+northern_cardinal = os.listdir("ML - Bird Classifier/northern_cardinal")
 for cardinal in northern_cardinal:
-    imag=cv2.imread("northern_cardinal/"+northern_cardinal)
+    imag = cv2.imread("ML - Bird Classifier/northern_cardinal/"+cardinal)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((50, 50))
     data.append(np.array(resized_image))
     labels.append(2)
 
-pigeon = os.listdir("pigeon")
+# creating dataset for pigeon
+pigeon = os.listdir("ML - Bird Classifier/pigeon")
 for pigeon in pigeon:
-    imag=cv2.imread("pigeon/"+pigeon)
+    imag = cv2.imread("ML - Bird Classifier/pigeon/"+pigeon)
     img_from_ar = Image.fromarray(imag, 'RGB')
     resized_image = img_from_ar.resize((50, 50))
     data.append(np.array(resized_image))
     labels.append(3)
 
-birds=np.array(data)
-labels=np.array(labels)
+# converting and saving dataset into numpy array
+birds = np.array(data)
+labels = np.array(labels)
 np.save("birds",birds)
 np.save("labels",labels)
 
-birds=np.load("birds.npy")
-labels=np.load("labels.npy")
+# loading dataset
+birds = np.load("birds.npy")
+labels = np.load("labels.npy")
 
-s=np.arange(birds.shape[0])
+# shuffling dataset
+s = np.arange(birds.shape[0])
 np.random.shuffle(s)
-birds=birds[s]
-labels=labels[s]
+birds = birds[s]
+labels = labels[s]
 
-num_classes=len(np.unique(labels))
-data_length=len(birds)
+num_species = len(np.unique(labels))
+data_length = len(birds)
+
+# creating train and test datasets
+x_train = birds[(int)(0.1*data_length):]
+x_test = birds[:(int)(0.1*data_length)]
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+
+train_length = len(x_train)
+test_length = len(x_test)
+
+# creating train and test label dataset
+y_train = labels[(int)(0.1*data_length):]
+y_test = labels[:(int)(0.1*data_length)]
+
+# one-hot encoding label dataset
+y_train = keras.utils.to_categorical(y_train, num_species)
+y_test = keras.utils.to_categorical(y_test, num_species)
+
+# building classification model
+# adding increasing filter sizes helps with adding more depth to images
+model= tf.keras.models.Sequential()
+model.add(Conv2D(filters=16,kernel_size=2,padding="same",activation="relu",input_shape=(50,50,3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32,kernel_size=2,padding="same",activation="relu"))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64,kernel_size=2,padding="same",activation="relu"))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(500,activation="relu"))
+model.add(Dropout(0.2))
+model.add(Dense(4,activation="softmax"))
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=50, epochs=100, verbose=1)
+
+# evaluating the model with the test dataset
+score = model.evaluate(x_test, y_test, verbose=1)
+print('\n', 'Test accuracy:', score[1])
+
+# saving model into JSON file 
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
+
---
+++ predict.py
@@ -0,0 +1,43 @@
+from keras.models import model_from_json
+import cv2
+from PIL import Image
+import numpy as np
+
+# load the model from the json file
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("model.h5")
+
+# predicting on a single image
+def convert_to_array(img):
+    im = cv2.imread(img)
+    img = Image.fromarray(im, 'RGB')
+    image = img.resize((50, 50))
+    return np.array(image)
+def get_bird_name(label):
+    if label == 0:
+        return "canada goose"
+    if label == 1:
+        return "blue jay"
+    if label == 2:
+        return "northern cardinal"
+    if label == 3:
+        return "pigeon"
+def predict_bird(file):
+    print("Predicting .................................")
+    ar = convert_to_array(file)
+    ar = ar/255
+    label = 1
+    a = []
+    a.append(ar)
+    a = np.array(a)
+    score = loaded_model.predict(a,verbose=1)
+    label_index = np.argmax(score)
+    acc = np.max(score)
+    animal = get_bird_name(label_index)
+    print("The predicted bird is a "+animal+" with accuracy = "+str(acc))
+
+predict_bird("pigeon_test1.jpg")
+
